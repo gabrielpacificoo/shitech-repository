ef9f9ade95e649b2dbf239a39c90818bd4004cdf
mahducosta <maria.guilherme@sptech.school>
2024-09-07 23:15:31 -0300

    Atualização do Codigo das tabelas

--- MySQL/ShitechBancoDados.sql
+++ MySQL/ShitechBancoDados.sql
@@ -22,35 +22,38 @@ INSERT INTO cadastro (nivel, usuario, cpf, email, senha) VALUES
 ('funcionario', 'Marcos Gustavo', '643.312.541-03','marcos.gustavo@empresa.com', 'empresa532');
 -- TODOS OS CPF'S, EMAILS E SENHAS UTILIZADOS SÃO FICTICIOS.
 SELECT * FROM cadastro;
+desc cadastro;
 
 CREATE TABLE champignon(
 id INT PRIMARY KEY AUTO_INCREMENT,
 dia date,
 horario TIME,
-temperaturaºC DECIMAL(3.1),
-umidade DECIMAL(3.1)	
+temperaturaºC DECIMAL(5,2),
+umidade DECIMAL(5,2)	
 );
 INSERT INTO champignon (dia, horario, temperaturaºC, umidade) VALUES
-('2024-09-01', '08:00:00', 22.5, 85.0),
-('2024-09-01', '08:30:00', 23.0, 80.0),
-('2024-09-01', '09:00:00', 21.5, 82.0),
-('2024-09-02', '14:30:00', 22.0, 83.0),
-('2024-09-02', '15:00:00', 23.5, 79.0),
-('2024-09-02', '15:30:00', 21.3, 83.0),
-('2024-09-03', '09:00:00', 22.0, 83.0),
-('2024-09-03', '09:30:00', 22.5, 83.0),
-('2024-09-03', '10:00:00', 23.0, 83.0),
-('2024-09-04', '12:00:00', 22.7, 83.0),
-('2024-09-04', '12:30:00', 23.1, 83.0),
-('2024-09-04', '13:00:00', 21.0, 83.0);
+('2024-09-01','08:00:00',22.5, 85.0),
+('2024-09-01','08:30:00',23.0, 80.0),
+('2024-09-01','09:00:00',21.5, 82.0),
+('2024-09-02','14:30:00',22.0, 83.0),
+('2024-09-02','15:00:00',23.5, 79.0),
+('2024-09-02','15:30:00',21.3, 83.0),
+('2024-09-03','09:00:00',22.0, 83.0),
+('2024-09-03','09:30:00',22.5, 83.0),
+('2024-09-03','10:00:00',23.0, 83.0),
+('2024-09-04','12:00:00',22.7, 83.0),
+('2024-09-04','12:30:00',23.1, 83.0),
+('2024-09-04','13:00:00',21.0, 83.0);
+
 SELECT * FROM champignon;
+desc champignon;
 
 CREATE TABLE shimeji(
 id INT PRIMARY KEY AUTO_INCREMENT,
 dia date,
 horario TIME,
-temperaturaºC DECIMAL(3.1),
-umidade DECIMAL(3.1)	
+temperaturaºC DECIMAL(5,2),
+umidade DECIMAL(5,2)	
 );
 
 INSERT INTO shimeji (dia, horario, temperaturaºC, umidade) VALUES
@@ -67,3 +70,4 @@ INSERT INTO shimeji (dia, horario, temperaturaºC, umidade) VALUES
 ('2024-09-04', '12:30:00', 23.1, 83.0),
 ('2024-09-04', '13:00:00', 21.0, 83.0);
 SELECT * FROM shimeji;
+desc shimeji;
